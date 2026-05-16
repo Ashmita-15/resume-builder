f85f9fb22786bdcb28147febb62d61382c47dcf1
Ashmita-15 <ashmitabarnwal53@gmail.com>
2026-05-16 16:32:55 +0530

    Update

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    try {
-
-        await mongoose.connect(process.env.MONGODB_URI, {
-            serverSelectionTimeoutMS: 5000
-        });
-
-        console.log("Database connected successfully");
-
-    } catch (error) {
-
-        console.log("MongoDB Connection Error:", error.message);
-
-        process.exit(1);
+  try {
+    if (mongoose.connections[0].readyState) {
+      return;
     }
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("MongoDB Connection Error:", error.message);
+
+    // DO NOT use process.exit() on Vercel
+    throw error;
+  }
 };
 
 export default connectDB;
